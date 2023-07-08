ad2fc0a0ddeb85f4f3734be28f3bf953fd5bfafb
numbasan-san <80220352+numbasan-san@users.noreply.github.com>
2023-07-08 17:24:32 -0400

    Add files via upload

---
+++ home/models.py
@@ -0,0 +1,43 @@
+
+from django.db import models
+
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
+
+class HomePage(Page):
+    summary = models.CharField(max_length = 140, default = '')
+    body = RichTextField(blank = True)
+    mary = RichTextField(blank = True)
+    corp = RichTextField(blank = True)
+    zelda = RichTextField(blank = True)
+ 
+    content_panels = Page.content_panels + [
+        FieldPanel('summary', classname = 'full'),
+        FieldPanel('body', classname = 'full'),
+        FieldPanel('mary', classname = 'full'),
+        FieldPanel('corp', classname = 'full'),
+        FieldPanel('zelda', classname = 'full')
+    ]
+
+class FlexPage(Page):
+    template = 'home/indicador.html'
+    subtitle = RichTextField(max_length = 100, null = True, blank = True)
+    body = RichTextField(blank = True)
+    content_panels = Page.content_panels + [
+        FieldPanel('subtitle'),
+        FieldPanel('body', classname = 'full'),
+    ]
+
+class SecondPage(Page):
+    template = 'home/informes.html'
+    subtitle = RichTextField(max_length = 100, null = True, blank = True)
+    body = RichTextField(blank = True)
+    content_panels = Page.content_panels + [
+        FieldPanel('subtitle'),
+        FieldPanel('body', classname = 'full'),
+    ]
+
+class Meta:
+    verbose_name = 'Indicador'
+    verbose_name_plural = 'Indicadores'
---
+++ web_sustent/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-_$b@#*gvi#6+mlid*k2zd$9-2x0jb@no2)z^(ibuie3+9%blen"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
