e28959fef69f35faa22945bddb90913f83c58a3a
numbasan-san <80220352+numbasan-san@users.noreply.github.com>
2023-08-10 12:04:11 -0400

    commit

--- home/models.py
+++ home/models.py
@@ -4,6 +4,8 @@ from wagtail.models import Page
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
 from wagtail.fields import StreamField
+from rest_framework.fields import Field
+from wagtail.images.api.fields import ImageRenditionField
 
 class HomePage(Page):
     summary = models.CharField(max_length = 140, default = '')
@@ -30,44 +32,9 @@ class Login(Page):
         FieldPanel('corp', classname = 'full'),
         FieldPanel('body', classname = 'full')
     ]
-    subpages_types = ["IndicadorDetalles"]
-    #  "IndicadorFormulario", "IndicadorGrafico", "IndicadorParametros"
 
 class Indicador(Page):
     template = 'home/indicador.html'
-    subtitle = RichTextField(max_length = 100, null = True, blank = True)
-    body = RichTextField(blank = True)
-    corp = RichTextField(blank = True)
-    content_panels = Page.content_panels + [
-        FieldPanel('subtitle'),
-        FieldPanel('corp', classname = 'full'),
-        FieldPanel('body', classname = 'full')
-    ]
-
-class IndicadorFormulario(Page):
-    template = 'home/form_validacion.html'
-    subtitle = RichTextField(max_length = 100, null = True, blank = True)
-    body = RichTextField(blank = True)
-    corp = RichTextField(blank = True)
-    content_panels = Page.content_panels + [
-        FieldPanel('subtitle'),
-        FieldPanel('corp', classname = 'full'),
-        FieldPanel('body', classname = 'full')
-    ]
-
-class IndicadorGrafico(Page):
-    template = 'home/indicador_grafico.html'
-    subtitle = RichTextField(max_length = 100, null = True, blank = True)
-    body = RichTextField(blank = True)
-    corp = RichTextField(blank = True)
-    content_panels = Page.content_panels + [
-        FieldPanel('subtitle'),
-        FieldPanel('corp', classname = 'full'),
-        FieldPanel('body', classname = 'full')
-    ]
-
-class IndicadorParametros(Page):
-    template = 'home/indicador_parametros.html'
     subtitle = RichTextField(max_length = 100, null = True, blank = True)
     body = RichTextField(blank = True)
     corp = RichTextField(blank = True)
@@ -95,15 +62,12 @@ class PMO(Page):
         FieldPanel('body', classname = 'full')
     ]
 
-class ParametroIndicador(Page):
-    template = 'home/test.html'
-    codigo = RichTextField(blank = False)
-    atributo = RichTextField(blank = False)
-    content_panels = Page.content_panels + [
-        FieldPanel('codigo'),
-        FieldPanel('atributo'),
-    ]
-
-class Meta:
-    verbose_name = 'Indicador'
-    verbose_name_plural = 'Indicadores'
+class ImageSerializationField(Field):
+    
+    def to_representation(self, value):
+        return {
+            "url": value.file.url,
+            "title": value.title,
+            "width": value.width,
+            "height": value.height,
+        }
